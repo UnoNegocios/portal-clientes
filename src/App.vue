cfe73064acc6e989cc194bd85211fbddbc839f4a
Joel de la Fuente <joeldlf@MacBook-Pro-de-Joel.local>
2022-12-27 23:22:29 -0600

    27dic3

--- src/App.vue
+++ src/App.vue
@@ -85,7 +85,7 @@ export default {
       return localStorage.getItem("first_login")
     },
   },
-  mounted(){
+  created(){
     setTimeout(() => {
       localStorage.removeItem("password_reset")
     }, 2000);
@@ -107,8 +107,10 @@ export default {
         localStorage.removeItem("first_login")
       }, 2000);
     }
+  },
+  mounted(){
     axios.get(process.env.VUE_APP_BACKEND_ROUTE + "api/v1/user/current").catch(error => {
-      localStorage.removeItem("session_token");
+    localStorage.removeItem("session_token");
       this.token = null
     });
   },
