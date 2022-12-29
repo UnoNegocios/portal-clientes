2918c1207ad1ee8aa829b950d270f8abde338aa6
Joel de la Fuente <joeldlf@MacBook-Pro-de-Joel.local>
2022-12-29 12:30:23 -0600

    final

--- public/register.js
+++
@@ -1,7 +0,0 @@
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-	navigator.serviceWorker.register('https://influencerpeach.unocrm.mx/service-worker.js')
-	.then(function(registration) { console.log('Service Worker Ready'); registration.update(); })
-	.catch(function(error) { console.log('Registration failed with ' + error); });
-  });
-}
