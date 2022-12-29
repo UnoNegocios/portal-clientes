fb588db3d4f1942f33b42783e927a3c54a9bea25
Joel de la Fuente <joeldlf@MacBook-Pro-de-Joel.local>
2022-12-29 12:20:55 -0600

    color top app 9

--- public/register.js
+++ public/register.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function() {
-	navigator.serviceWorker.register('https://influencerpeach.unocrm.mx-service-worker.js')
+	navigator.serviceWorker.register('https://influencerpeach.unocrm.mx/service-worker.js')
 	.then(function(registration) { console.log('Service Worker Ready'); registration.update(); })
 	.catch(function(error) { console.log('Registration failed with ' + error); });
   });
